4f07ed8a70cdf20f74f75d45c238c7f6f2882046
Keshav Achint <43784418+keshavbawa@users.noreply.github.com>
2023-07-06 10:03:22 +0530

    Code

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
+
---
+++ DatingApp/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Entities
+{
+	public class AppUser
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+	}
+}
+
